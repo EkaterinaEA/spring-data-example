b24457963484bcb3b5a523872a29a2956eddd116
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-27 19:51:18 -0500

    change import static org.mockito.Mockito.*; import static org.junit.jupiter.api.Assertions.*; in test service in multi-store project

--- src/test/com/rightline/service/AddressServiceTest.java
+++ src/test/com/rightline/service/AddressServiceTest.java
@@ -13,8 +13,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.doNothing;
+
 
 @SpringJUnitConfig(ApplicationRunner.class)
 class AddressServiceTest {
--- src/test/com/rightline/service/OrderServiceTest.java
+++ src/test/com/rightline/service/OrderServiceTest.java
@@ -11,11 +11,15 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.doNothing;
+
 
 @SpringJUnitConfig(ApplicationRunner.class)
 class OrderServiceTest {
