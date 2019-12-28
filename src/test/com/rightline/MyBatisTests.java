9db650ae5b72f668dceed9be98802e9e26d5afe2
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-27 19:46:09 -0500

    change import static org.assertj.core.api.Assertions.assertThat; in entity in jdbc project

--- src/test/com/rightline/MyBatisTests.java
+++ src/test/com/rightline/MyBatisTests.java
@@ -1,6 +1,6 @@
 package com.rightline;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.rightline.dao.LegoSetRepository;
