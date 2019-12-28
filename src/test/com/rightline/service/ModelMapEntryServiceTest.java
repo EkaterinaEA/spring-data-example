fd752858ba27d3d5c7f474e0a3dda60ce828fccb
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-27 20:16:00 -0500

    change import static org.mockito.Mockito.*; import static org.junit.jupiter.api.Assertions.*; in test service in jdbc project

--- src/test/com/rightline/service/ModelMapEntryServiceTest.java
+++ src/test/com/rightline/service/ModelMapEntryServiceTest.java
@@ -12,11 +12,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.doNothing;
 
 @SpringJUnitConfig(ApplicationRunner.class)
 class ModelMapEntryServiceTest {
