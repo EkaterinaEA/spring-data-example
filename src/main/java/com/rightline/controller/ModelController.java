c8beef39c03026336e541a67ee9c406875f3fa01
EkaterinaEA <krasnovaea95@gmail.com>
2019-12-30 14:01:55 -0500

    change ModelController

--- src/main/java/com/rightline/controller/ModelController.java
+++ src/main/java/com/rightline/controller/ModelController.java
@@ -17,56 +17,55 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/model")
 public class ModelController {
 
     @Autowired
-    ModelService modelService;
+    private ModelService modelService;
 
     @PostMapping
-    ResponseEntity<Model> save(@RequestBody Model model) {
-        Model savedModel = modelService.save(model);
-        if (savedModel != null) {
-            return new ResponseEntity<>(savedModel, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    ResponseEntity<Model> save(@RequestBody final Model model) {
+        final Model savedModel = modelService.save(model);
+        return Optional.ofNullable(savedModel).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
     @PutMapping
-    ResponseEntity<Model> update(@RequestBody Model model) {
-        Model savedModel = modelService.update(model);
-        if (savedModel != null) {
-            return new ResponseEntity<>(savedModel, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    ResponseEntity<Model> update(@RequestBody final Model model) {
+        final Model updatedModel = modelService.update(model);
+        return Optional.ofNullable(updatedModel).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
     @ApiParam("test id description")
-    @GetMapping({"/{id}", ""})
-    ResponseEntity findById(@PathVariable(required = false) Integer id) {
-        if (id == null) {
-            return new ResponseEntity<List>(modelService.findAll(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<Model>(modelService.findById(id), HttpStatus.OK);
-        }
+    @GetMapping({"/{id}"})
+    ResponseEntity findById(@PathVariable(required = false) final Integer id) {
+        final Model foundModel = modelService.findById(id);
+        return Optional.ofNullable(foundModel).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
+    }
+
+    @GetMapping({""})
+    ResponseEntity findAll() {
+        final List<Model> foundModels = modelService.findAll();
+        return Optional.ofNullable(foundModels).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
     @DeleteMapping("/{id}")
-    ResponseEntity<Model> deleteById(@PathVariable Integer id) {
+    ResponseEntity<Model> deleteById(@PathVariable final Integer id) {
         modelService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping()
-    ResponseEntity<Model> delete(@RequestBody Model model) {
+    ResponseEntity<Model> delete(@RequestBody final Model model) {
         modelService.delete(model);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/find-all-models-by-legosetid/{cartId}")
-    ResponseEntity<Model> findAllModelsByLegoSetId(@PathVariable Integer legoSetId) {
-        return new ResponseEntity(modelService.findAllModelsByLegoSetId(legoSetId), HttpStatus.OK);
+    ResponseEntity<List<Model>> findAllModelsByLegoSetId(@PathVariable final Integer legoSetId) {
+        final List<Model> foundModels = modelService.findAllModelsByLegoSetId(legoSetId);
+        return Optional.ofNullable(foundModels).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 }
