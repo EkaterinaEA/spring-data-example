961d2f1d7c31019c57fb45f7ca6532e260726a41
EkaterinaEA <krasnovaea95@gmail.com>
2020-02-08 19:52:35 -0500

    Quartz

--- src/main/java/com/rightline/springquartz/TriggerJobMerchant.java
+++ src/main/java/com/rightline/springquartz/TriggerJobMerchant.java
@@ -21,36 +21,6 @@ public class TriggerJobMerchant {
                 .withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
         scheduler.scheduleJob(jobDetail, trigger);
 
-        /*
-        // обход по всем группам
-        for (String groupName : config.getJobGroupNames()) {
-            // обход по всем job группы
-            for (JobKey jobKey : config.getJobKeys(GroupMatcher.jobGroupEquals(groupName))) {
-                String jobName = jobKey.getName();
-                String jobGroup = jobKey.getGroup();
-                if (jobName.equals("jobDetailMerchantAction")) {
-
-                    // нашли все триггеры
-                    List<Trigger> triggers = (List<Trigger>) config.getTriggersOfJob(jobKey);
-                    if (triggers.isEmpty()) {
-                        CronTrigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger2", "group2")
-                                .withSchedule(CronScheduleBuilder.cronSchedule(JobSchedule.getSchedule().getCron())).build();
-                        config.scheduleJob(jobDetail, trigger);
-                    } else {
-                        for (Trigger tr : triggers) {
-                            CronTrigger trigger = (CronTrigger) tr;
-                            if (tr instanceof TriggerJobMerchant && tr.getClass().getName().equals("TriggerJobMerchant")) {
-                                trigger = TriggerBuilder.newTrigger().withIdentity("trigger2", "group2")
-                                        .withSchedule(CronScheduleBuilder.cronSchedule(JobSchedule.getSchedule().getCron())).build();
-                            }
-                            config.scheduleJob(jobDetail, trigger);
-                        }
-                    }
-                }
-            }
-        }
-
-         */
         scheduler.start();
     }
 }
